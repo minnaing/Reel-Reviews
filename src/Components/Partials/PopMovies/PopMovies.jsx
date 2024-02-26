8a7c362367829fdfc0d89e439adea0e94ab1d3cc
Terrell D Lemons <LemonsTerrell2021@gmail.com>
2024-02-25 21:37:01 -0800

    Added pop-movies.css

--- src/Components/Partials/PopMovies/PopMovies.jsx
+++ src/Components/Partials/PopMovies/PopMovies.jsx
@@ -1,4 +1,6 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
+
+import './pop-movies.css'
 
 // // DEFINE API URL FOR FETCHING POPULAR MOVIES FROM THE MOVIE DATABASE API
 const BASE_URL = "https://api.themoviedb.org/3";
@@ -7,12 +9,10 @@ const BASE_URL = "https://api.themoviedb.org/3";
 const API_KEY = "ff0abd9e4de81e5a3e858b6b617453fa";
 
 const PopMovies = () => {
-
   // State for storing popular movies
-  const [popularMovies, setPopularMovies] = useState([]); 
+  const [popularMovies, setPopularMovies] = useState([]);
 
   useEffect(() => {
-    
     // Fetch popular movies on component mount
     fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}&language=en-US&page=1`)
       .then((res) => res.json())
@@ -23,22 +23,24 @@ const PopMovies = () => {
 
   return (
     <div>
-    {/* Displaying popular movies */}
-    <div className="popular-movies">
+      {/* Displaying popular movies */}
+      <div className="popular-movies">
         <h2 style={{ textAlign: "center" }}>Popular Movies</h2>
         <div className="movie-grid">
-          {popularMovies.map(movie => (
+          {popularMovies.map((movie) => (
             <div key={movie.id} className="movie-card">
-              <img 
-                className='pop-movies'
-                src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} 
-                alt={movie.title} />
+              <img
+                className="pop-movies"
+                src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                alt={movie.title}
+              />
               <h3>{movie.title}</h3>
             </div>
           ))}
         </div>
-      </div></div>
-  )
-}
+      </div>
+    </div>
+  );
+};
 
 export default PopMovies;
