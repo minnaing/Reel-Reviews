54341287bcac226aa3150be97b555a7e41ff0bc8
ggwanggg <37925994+ggwanggg@users.noreply.github.com>
2024-02-26 17:21:15 -0800

    debug

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,6 @@
 const express = require("express");
 const nodemailer = require("nodemailer");
 const cors = require("cors");
-const bodyParser = require("body-parser");
 
 // Initialize the express application
 const app = express();
