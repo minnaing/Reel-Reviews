36c859446d77c633becce3c87ffca3bbca888ba5
Terrell D Lemons <LemonsTerrell2021@gmail.com>
2024-03-30 20:46:30 -0700

    Deploying to Heroku

--- client/src/Components/Navbar/StickyNav.jsx
+++ client/src/Components/Navbar/StickyNav.jsx
@@ -1,32 +1,32 @@
-import React from "react";
-import { useNavigate } from "react-router-dom";
+// import React from "react";
+// import { useNavigate } from "react-router-dom";
 
-function StickyNav() {
-  let navigate = useNavigate();
+// function StickyNav() {
+//   let navigate = useNavigate();
 
-  function handleClick() {
-    navigate("/reviews");
-  }
+//   function handleClick() {
+//     navigate("/reviews");
+//   }
 
-  return (
-    <div>
-      <div className="App-1">
-        <div className="sticky-header">
-          <h1>Welcome to Movie Explorer</h1>
-          <h2>Type in the Search Bar to find the Movies you Love.</h2>
-          <p>
-            Discover your next favorite movie with our easy-to-use search feature. Just start typing, and let us do the rest!
-          </p>
-          <p>
-            Our navigation bar will stay with you, ensuring seamless browsing through our vast movie library.
-          </p>
-          <button type="button" style={{ color: "white" }} onClick={handleClick}>
-            Start Exploring
-          </button>
-        </div>
-      </div>
-    </div>
-  );
-}
+//   return (
+//     <div>
+//       <div className="App-1">
+//         <div className="sticky-header">
+//           <h1>Welcome to Movie Explorer</h1>
+//           <h2>Type in the Search Bar to find the Movies you Love.</h2>
+//           <p>
+//             Discover your next favorite movie with our easy-to-use search feature. Just start typing, and let us do the rest!
+//           </p>
+//           <p>
+//             Our navigation bar will stay with you, ensuring seamless browsing through our vast movie library.
+//           </p>
+//           <button type="button" style={{ color: "white" }} onClick={handleClick}>
+//             Start Exploring
+//           </button>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
 
-export default StickyNav;
+// export default StickyNav;
