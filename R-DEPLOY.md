13d92b353b4110554d1c98f86d8a1b4bd2be7ae1
Terrell D Lemons <LemonsTerrell2021@gmail.com>
2024-03-30 21:18:41 -0700

    Deploying to Heroku

---
+++ R-DEPLOY.md
@@ -0,0 +1,79 @@
+# Deploying a React App
+
+Here's a step-by-step guide to get our React app published on GitHub Pages. Before we start, make sure we have a GitHub account and that our React app is in a Git repository.
+
+```bash
+ls -a
+```
+
+This will list all files, including hidden files, and will tell us if we have a `.git` file in our project.
+
+## Step 1: Add Homepage to `package.json`
+
+In our React app's `package.json`, add a `homepage` field. Replace `ourusername` with our GitHub username, and `our-app-name` with the name of our GitHub repository.
+
+```json
+"homepage": "https://lemonmadedesigns.github.io/Reel-Reviews/#/",
+```
+
+## Step 2: Install `gh-pages`
+
+Run the following command in our project directory to install the `gh-pages` package, which helps create a gh-pages branch on GitHub and publish our app to it.
+
+```bash
+npm install --save gh-pages
+```
+
+## Step 3: Add Deployment Scripts
+
+In the same `package.json` file, add the following scripts to automate the deployment process.
+
+```json
+"scripts": {
+  "predeploy": "npm run build",
+  "deploy": "gh-pages -d build",
+  ...
+},
+```
+
+## Step 4: Create a GitHub Repository
+
+We have already create a repository on GitHub and pushed our project to it. We can re-initialize it with:
+
+```bash
+git init
+git add .
+git commit -m "Initial commit"
+```
+
+Then follow the instructions on GitHub to add a remote and push our code.
+
+## Step 5: Deploy to GitHub Pages
+
+Now, we can deploy our app to GitHub Pages by running:
+
+```bash
+npm run deploy
+```
+
+This command builds the app, creates/updates the `gh-pages` branch, and pushes the build to that branch.
+
+## Step 6: Configure GitHub Pages on GitHub
+
+- Go to our repository on GitHub and navigate to the "Settings" tab.
+- Scroll down to the "Pages" section.
+- Under "Source", select the `gh-pages` branch and save.
+
+Our app should now be live at [Reel-Reviews](https://lemonmadedesigns.github.io/Reel-Reviews/).
+
+## Step 7: Verify Deployment
+
+After the deployment, visit [Reel-Reviews](https://lemonmadedesigns.github.io/Reel-Reviews/) to see our deployed React app. It may take a few minutes for the changes to be visible.
+
+### Notes
+
+- If our React app uses React Router, we might need to adjust our routing for GitHub Pages. We have to consider using `HashRouter` instead of `BrowserRouter` for client-side routing to work correctly on GitHub Pages.
+
+- Ensure that our repository is public if we want our site to be accessible by everyone.
+
+This guide provides a basic overview of deploying our React app to GitHub Pages. Depending on our project's specifics, and possible pivots, we might need to adjust some steps.
---
+++ server.js
@@ -0,0 +1,112 @@
+// Import necessary modules
+const express = require("express");
+const nodemailer = require("nodemailer");
+const cors = require("cors");
+const path = require("path");
+
+//Helmet.js use for Securing  Express HTTP Headers
+const helmet = require("helmet");
+
+// In API routes or getServerSideProps
+// import { serialize } from 'cookie';
+// import type { NextApiRequest, NextApiResponse } from 'next';
+
+// export default function handler(req: NextApiRequest, res: NextApiResponse) {
+//   res.setHeader('Set-Cookie', serialize('token', 'yourTokenValue', { path: '/' }));
+//   res.status(200).json({ message: 'Cookie set' });
+// }
+
+// Initialize the express application
+const app = express();
+//
+// Use environment variable for port or default to 9999
+const PORT = process.env.PORT || 9999;
+
+// Helmet Security Header Set Up. X-Content-Type-Options by default on
+// app.use(
+//   helmet({
+//     strictTransportSecurity: {
+//       maxAge: 15552000, // Set to one year in seconds
+//       includeSubDomains: true, // Include subdomains
+//     },
+//     xFrameOptions: { action: "sameorigin" },
+//     referrerPolicy: {
+//       policy: "no-referrer",
+//     },
+//     contentSecurityPolicy: {
+//       directives: {
+//         defaultSrc: ["'self'"],
+//         // Allows scripts from Google Charts and inline scripts
+//         scriptSrc: ["'self'", "'unsafe-inline'", "https://www.gstatic.com"],
+//         // Allows styles from self, inline styles, and Google Fonts
+//         styleSrc: ["'self'", "'unsafe-inline'", "https://fonts.googleapis.com"],
+//         // Allows fonts from Google Fonts
+//         fontSrc: ["'self'", "https://fonts.gstatic.com"],
+//         // Allows images from self and data URLs
+//         imgSrc: ["'self'", "data:"],
+//         // Specifies the URLs that can be used to connect (fetch, XMLHttpRequest, WebSocket, etc.)
+//         connectSrc: ["'self'", "https://api.themoviedb.org"],
+//         // Add other directives as needed
+//       },
+//     },
+//   })
+// );
+
+// Middleware setup
+app.use(cors()); // Enable CORS for all origins (configure as needed for your environment)
+app.use(express.json()); // Parse JSON bodies (as sent by API clients)
+
+// Serve the favicon
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico'))); // Update the path according to your favicon's location
+
+// Serve static files from the React app build directory and any other static assets
+app.use(express.static(path.join(__dirname, "client/build")));
+app.use(express.static("public")); // Serve static files from 'public' directory
+
+// POST endpoint to send emails
+app.post("/send-email", async (req, res) => {
+  // Extract data from request body
+  const { name, email, message } = req.body; // Adjust these fields according to your form
+  console.log("Received request:", { name, email, message }); // Log received data for debugging
+
+  // Setup Nodemailer transporter using Gmail (configure according to your email provider)
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "reelreviewstest@gmail.com", // Your email
+      pass: "halyoqyczhmgbxrd", // Your email account password or app-specific password
+    },
+  });
+
+  // Email options
+  const mailOptions = {
+    from: "reelreviewstest@gmail.com", // Fixed sender email address
+    to: "lchan2021@csu.fullerton.edu", // Fixed recipient email address
+    subject: `Message from ${name} (${email})`, // Combine sender's name and email in the subject
+    text: message, // Email body as provided in the message field
+  };
+
+  // Attempt to send the email
+  try {
+    let emailResult = await transporter.sendMail(mailOptions);
+    console.log("Email sent successfully", emailResult); // Log the success result for debugging
+
+    // Inside your try block, after successfully sending the email
+    res.json({ message: "Email sent successfully" }); // Respond with JSON
+  } catch (error) {
+    console.error("Error sending email:", error.message); // Log detailed error information
+    res.status(500).send("Failed to send email"); // Respond with error upon failure
+  }
+});
+
+// Handles any requests that don't match the ones above
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/build", "index.html"));
+});
+
+// Start the server
+app.listen(PORT, () =>
+  console.log(
+    `Server running on port ${PORT}\n App running on https://localhost:3000`
+  )
+);
