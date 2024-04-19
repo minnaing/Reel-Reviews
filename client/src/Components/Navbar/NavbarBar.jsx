6510f9f2856e707492273f53af37ab068e328958
Terrell D Lemons <LemonsTerrell2021@gmail.com>
2024-04-18 18:26:11 -0700

    Deploying to Heroku

--- client/src/Components/Navbar/NavbarBar.jsx
+++ client/src/Components/Navbar/NavbarBar.jsx
@@ -10,7 +10,6 @@ const NavbarBar = () => {
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
-<<<<<<< HEAD
     return (
         <Navbar expand="lg" style={{ position: 'sticky', top: 0, zIndex: 1000 }} className="bg-body-tertiary mb-3">
             <Container fluid>
@@ -57,59 +56,6 @@ const NavbarBar = () => {
             </Container>
         </Navbar>
     );
-=======
-  return (
-    <Navbar
-      expand="lg"
-      style={{ position: "sticky", top: 0, zIndex: 1000 }}
-      className="bg-body-tertiary mb-3"
-    >
-      <Container fluid>
-        <Navbar.Brand as={Link} to="/" href="#">
-          <ReelLogo />
-          Reel Reviews
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={handleShow} />
-        <Navbar.Offcanvas
-          id="offcanvasNavbar"
-          aria-labelledby="offcanvasNavbarLabel"
-          placement="start"
-          show={show}
-          onHide={handleClose}
-        >
-          <Offcanvas.Header closeButton>
-            <Offcanvas.Title id="offcanvasNavbarLabel">
-              <Navbar.Brand as={Link} to="/" href="#">
-                <ReelLogo />
-                Reel Reviews
-              </Navbar.Brand>
-            </Offcanvas.Title>
-          </Offcanvas.Header>
-          <Offcanvas.Body>
-            <Nav className="justify-content-end flex-grow-1 pe-3">
-              <Nav.Link as={Link} to="/" onClick={handleClose}>
-                Home
-              </Nav.Link>
-              <Nav.Link as={Link} to="/reviews" onClick={handleClose}>
-                Reviews
-              </Nav.Link>
-              <Nav.Link as={Link} to="/region_reviews" onClick={handleClose}>
-                Region_Reviews
-              </Nav.Link>
-              <Nav.Link as={Link} to="/about" onClick={handleClose}>
-                About
-              </Nav.Link>
-
-              <Nav.Link as={Link} to="/contact" onClick={handleClose}>
-                Contact
-              </Nav.Link>
-            </Nav>
-          </Offcanvas.Body>
-        </Navbar.Offcanvas>
-      </Container>
-    </Navbar>
-  );
->>>>>>> origin/main
 };
 
 export default NavbarBar;
